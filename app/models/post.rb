46ce656258dfe4967bae4d99bc5f6edd695eec77
JustinMitchell <mitchelljm04@gmail.com>
2022-12-08 10:35:47 -0500

    commit tree 35ba4068f5773d164bd5117116059bdab547865c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true 
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction","Non-Fiction"]}
+    validate :error_facts 
+
+    def error_facts
+        if title == "True Facts"
+            errors.add(:title, "This title is click bate.")
+        end
+    end
 end
